5ca4f93deebb8d1dd52f3ba67fb370451ec42620
krisi01 <chrisisbeast@abv.bg>
2021-01-05 05:02:59 -0800

    practicing express

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,70 +1,31 @@
 import React, { useState } from "react";
-import "./App.css";
-import axios from "axios";
-export default function App() {
-  let [usernameRegister, setUsernameRegister] = useState("");
-  let [passwordRegister, setPasswordRegister] = useState("");
-  let [usernameLogin, setUsernameLogin] = useState("");
-  let [passwordLogin, setPasswordLogin] = useState("");
-  let [currentUser, setCurrentUser] = useState();
-  const register = () => {
-    axios
-      .post("http://localhost:4000/register", {
-        username: usernameRegister,
-        password: passwordRegister,
-      })
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
-  };
-  const login = () => {
-    axios
-      .post("http://localhost:4000/login", {
-        username: usernameLogin,
-        password: passwordLogin,
-      })
-      .then((res) => getUser)
-      .catch((err) => console.log(err));
-  };
-
-  const getUser = () => {
-    axios
-      .get("http://localhost:4000/login")
-      .then((res) => {
-        setCurrentUser(res.data);
-      })
-      .catch((err) => console.log(err));
-  };
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import classes from "./App.module.css";
+import Register from "./components/register";
+import Login from "./components/login";
+const App = (props) => {
   return (
-    <div className="app">
-      <div>
-        <div className="register">
-          <input
-            type="text"
-            onChange={(e) => setUsernameRegister(e.target.value)}
-            placeholder="username"
-          />
-          <input
-            type="password"
-            onChange={(e) => setPasswordRegister(e.target.value)}
-            placeholder="password"
-          />
-          <button onClick={register}>Register</button>
-        </div>
+    <Router>
+      <div className={classes.App}>
+        <nav>
+          <ul>
+            <li>
+              <Link className={classes.Link} to="/">
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link className={classes.Link} to="/register">
+                Register
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/register" exact component={Register} />
+        <Route path="/" exact component={Login} />
       </div>
-      <div className="login">
-        <input
-          type="text"
-          onChange={(e) => setUsernameLogin(e.target.value)}
-          placeholder="username"
-        />
-        <input
-          type="password"
-          onChange={(e) => setPasswordLogin(e.target.value)}
-          placeholder="password"
-        />
-        <button onClick={login}>Login</button>
-        <p>current user: {currentUser}</p>
-      </div>
-    </div>
+    </Router>
   );
-}
+};
+
+export default App;
