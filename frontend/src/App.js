599d1c9fda890e7c95ff1f5315fcbf6af66738d8
krisi01 <chrisisbeast@abv.bg>
2021-01-12 22:41:01 +0200

    starting the project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,11 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
-import classes from "./App.module.css";
-import axios from "axios";
-import Register from "./components/register";
-import Login from "./components/login";
+
 const App = (props) => {
   
   return (
-    <Router>
-      <div className={classes.App}>
-        <nav>
-          <ul>
-            <li>
-              <Link className={classes.Link} to="/">
-                Login
-              </Link>
-            </li>
-            <li>
-              <Link className={classes.Link} to="/register">
-                Register
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <Route path="/user" exact render={() => <h1>hey</h1>} />
-        <Route path="/register" exact component={Register} />
-        <Route path="/" exact component={Login} />
-      </div>
-    </Router>
+    <div>
+      
+    </div>
   );
 };
 
