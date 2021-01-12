82934b9bfde96863ce6250feff14791fe57bb2da
krisi01 <chrisisbeast@abv.bg>
2021-01-13 00:34:47 +0200

    overlay fix

--- frontend/src/components/Products/Products.js
+++ frontend/src/components/Products/Products.js
@@ -1,34 +1,43 @@
 import classes from "./Products.module.css";
 import React from "react";
 import Product from "./Product";
+import image from "../../assets/shoes.png";
 export default function Products() {
   return (
-    <div className={classes.Products}>
-      <Product
-        source="http://images.amazon.com/images/P/0596004613.01._PE30_PI_SCMZZZZZZZ_.jpg"
-        info="This book is awesome!"
-        added={() => alert("added to cart")}
-      />
-      <Product
-        source="http://images.amazon.com/images/P/0596004613.01._PE30_PI_SCMZZZZZZZ_.jpg"
-        info="This book is awesome!"
-        added={() => alert("added to cart")}
-      />
-      <Product
-        source="http://images.amazon.com/images/P/0596004613.01._PE30_PI_SCMZZZZZZZ_.jpg"
-        info="This book is awesome!"
-        added={() => alert("added to cart")}
-      />
-      <Product
-        source="http://images.amazon.com/images/P/0596004613.01._PE30_PI_SCMZZZZZZZ_.jpg"
-        info="This book is awesome!"
-        added={() => alert("added to cart")}
-          />
-          <Product
-        source="http://images.amazon.com/images/P/0596004613.01._PE30_PI_SCMZZZZZZZ_.jpg"
-        info="This book is awesome!"
-        added={() => alert("added to cart")}
-      />
-    </div>
+    <>
+      <div style={{ position: "relative" }}>
+        <header>
+          <img src={image} alt="default" className={classes.mainImage} />
+          <p className={classes.Quote}>We have the best shoes on the market</p>
+        </header>
+      </div>
+      <div className={classes.Products}>
+        <Product
+          source="http://images.amazon.com/images/P/0596004613.01._PE30_PI_SCMZZZZZZZ_.jpg"
+          info="This book is awesome!"
+          added={() => alert("added to cart")}
+        />
+        <Product
+          source="http://images.amazon.com/images/P/0596004613.01._PE30_PI_SCMZZZZZZZ_.jpg"
+          info="This book is awesome!"
+          added={() => alert("added to cart")}
+        />
+        <Product
+          source="http://images.amazon.com/images/P/0596004613.01._PE30_PI_SCMZZZZZZZ_.jpg"
+          info="This book is awesome!"
+          added={() => alert("added to cart")}
+        />
+        <Product
+          source="http://images.amazon.com/images/P/0596004613.01._PE30_PI_SCMZZZZZZZ_.jpg"
+          info="This book is awesome!"
+          added={() => alert("added to cart")}
+        />
+        <Product
+          source="http://images.amazon.com/images/P/0596004613.01._PE30_PI_SCMZZZZZZZ_.jpg"
+          info="This book is awesome!"
+          added={() => alert("added to cart")}
+        />
+      </div>
+    </>
   );
 }
