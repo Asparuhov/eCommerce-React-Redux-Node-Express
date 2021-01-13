247021100908d1bb2980965cc29a4adbc746a140
krisi01 <chrisisbeast@abv.bg>
2021-01-13 17:20:21 +0200

    registering user to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,41 @@
 const express = require("express");
-
+const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require("./userModel");
+const bcrypt = require("bcryptjs");
 const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
-const posts = [
-  {
-    name: "Chris",
-    title: "Post1",
-  },
-  {
-    name: "Chris1",
-    title: "Post2",
-  },
-];
-
-app.get("/posts", (req, res) => {
-  res.json(posts);
+app.post("/register", async (req, res) => {
+  let { username, email, password } = req.body;
+  const salt =  await bcrypt.genSalt();
+  const hashedPass = await bcrypt.hash(password, salt);
+  const newUser = new User({
+    username: username,
+    email: email,
+    password: hashedPass
+  });
+  newUser
+    .save()
+    .then((res) => console.log("User created"))
+    .catch((err) => console.log(err));
 });
 
-app.listen(4000);
+mongoose
+  .connect(
+    "mongodb+srv://Chris:Krisi0143171864a@cluster0.d3hu6.mongodb.net/users?retryWrites=true&w=majority",
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }
+  )
+  .then((res) => {
+    app.listen(4000);
+    console.log("Connected to database");
+  })
+  .catch((err) => console.log(err));
--- frontend/src/containers/Register/Register.js
+++ frontend/src/containers/Register/Register.js
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Register.module.css";
 import { Link } from "react-router-dom";
+import axios from "axios";
 const Register = (props) => {
+  let [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const register = () => {
+    axios
+      .post("http://localhost:4000/register", user)
+      .then("User sent to backend")
+      .catch((err) => console.log(err));
+  };
   return (
     <div className={classes.Register}>
       <h1>Register</h1>
-      <input placeholder="nickname" type="text" />
-      <input placeholder="email" type="email" />
-      <input placeholder="password" type="password" />
-      <button>Register</button>
+      <input
+        placeholder="nickname"
+        type="text"
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+      />
+      <input
+        placeholder="email"
+        type="email"
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+      />
+      <input
+        placeholder="password"
+        type="password"
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+      />
+      <button onClick={() => register()}>Register</button>
       <p>
-        Already registered? <Link to="/login">Login here</Link>
+        Already registered?
+        <Link className={classes.link} to="/login">
+          Login here
+        </Link>
       </p>
     </div>
   );
