63a3f3298c3a4beeb77ecba61d7836b2ffd126d0
krisi01 <chrisisbeast@abv.bg>
2021-01-14 14:52:20 +0200

    fixing products design

--- frontend/src/components/Products/Product.js
+++ frontend/src/components/Products/Product.js
@@ -6,7 +6,7 @@ export default function Product(props) {
     <div className={classes.product}>
       <img className={classes.image} src={props.source} alt="default" />
       <p className={classes.info}>{props.info}</p>
-      <p className={classes.info}>{props.price}</p>
+      <p className={classes.price}>${props.price}</p>
       <button className={classes.button} onClick={props.added}>
         Add to cart
       </button>
